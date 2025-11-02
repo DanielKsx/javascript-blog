9218c594d7335ab770a95d390e4892649aa36528
Daniel Kowalczyk <kostex2@gmail.com>
2025-11-02 11:26:57 +0000

    add class active to to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,27 +6,30 @@
 
 
 const titleClickHandler = function(event){
-  console.log(event);
+  const clickedElement = this;
+  console.log('clickedElement (with plus): ' + clickedElement);
 
-  /* remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
  const activeLinks = document.querySelectorAll('titles a.active');
 
     for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-  /* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-  /* remove class 'active' from all articles */
+/* [DONE] remove class 'active' from all articles */
  const activeArticles = document.querySelectorAll('.post.active');
 
     for(let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-  /* get 'href' attribute from the clicked link */
+/* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+/* find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
+/* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
