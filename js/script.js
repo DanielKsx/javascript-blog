15ee15ffc7efef4dad1a9234b43bb007fcd7d206
Daniel Kowalczyk <kostex2@gmail.com>
2025-11-02 12:07:58 +0000

    Implement titleClickHandler to handle article switching on link click

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@
 }); */
 
 
-
 const titleClickHandler = function(event){
+  event.preventDefault();
+
   const clickedElement = this;
   console.log('clickedElement (with plus): ' + clickedElement);
 
 /* [DONE] remove class 'active' from all article links  */
- const activeLinks = document.querySelectorAll('titles a.active');
+ const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
@@ -25,11 +26,16 @@ const titleClickHandler = function(event){
     for(let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-/* get 'href' attribute from the clicked link */
+/* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector;', articleSelector);
 
-/* find the correct article using the selector (value of 'href' attribute) */
+/* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle;', targetArticle);
 
-/* add class 'active' to the correct article */
+/* [DONE]add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -37,33 +43,3 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-
-
-
-
-
-
-
-
-
-/* sciaga
-const nazwaList = document.querySelector('.klasa');
-
-const items = nazwaList.querySelectorAll('lista np');
-
-const dowolnaNazwa = function() {
-    console.log(this);
-    const clickedElement = this;
-
-    clickedElement.classList.add('klasa bez kropki')
-
-    const time = clickedElement.getAttribute("atrybut");
-    console.log(time);
-
-};
-for(const item of items) {
-
-    item.addEventListener('click', dowolnaNazwa)
-}  
-    
-*/
